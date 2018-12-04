cfd54ef444cccd71cff398619dec35ad84cefaf1
developer-soc2 <developer.soc2@gmail.com>
2018-12-04 19:33:25 +0800

    test

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const { WebhookClient, Card, } = require('dialogflow-fulfillment');
 //Create an instance of express server
 const app = express().use(bodyParser.json());
 
-expressApp.post('/fulfillment', functions.https.onRequest((request, response) => {
+app.post('/fulfillment', functions.https.onRequest((request, response) => {
     const agent = new WebhookClient({ request, response });
-    
+
     function welcome(agent) {
         agent.add(`Welcome to my agent in Heroku!`);
         agent.add(new Card({
